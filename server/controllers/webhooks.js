1e21117befddb7345a501e3bb2334d764325ff9e
Ramsaran17 <ramsaran1701@gmail.com>
2026-07-10 12:50:14 +0530

    Fix Clerk webhook

--- server/controllers/webhooks.js
+++ server/controllers/webhooks.js
@@ -21,7 +21,7 @@ export const clerkWebhooks = async (req, res) => {
             case 'user.created': {
                const userData = {
                 _id: data.id,
-                email: data.email_addresses[0].email_addresses,
+                email: data.email_addresses[0].email_address,
                 name : data.first_name + " " +data.last_name,
                 image: data.image_url,
                 resume: ''
@@ -32,7 +32,7 @@ export const clerkWebhooks = async (req, res) => {
             }
             case 'user.updated': {
                  const userData = {
-                email: data.email_addresses[0].email_addresses,
+                email: data.email_addresses[0].email_address,
                 name : data.first_name + " " +data.last_name,
                 image: data.image_url,
                }
@@ -54,8 +54,10 @@ export const clerkWebhooks = async (req, res) => {
         }
 
     } catch (error) {
-        console.log(error.message);
-        res.josn({success:false,message:'Webhooks Error'})
+    console.log(error);
 
-    }
-}
+    res.status(500).json({
+        success: false,
+        message: error.message
+    });
+}}
